c43adbf0c7aee44c8d51535aa012e158ce5cdb74
VoilaSecari <lifeaslaurafilip@gmail.com>
2025-05-28 00:24:03 +0200

    inizio filtraggio e disposizione di use state

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
 import { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/index.css";
+import movies from "./assets/data/movies";
 
 export default function App() {
-  return <h1>Bella lì</h1>;
+  //useState
+  const [selectedGenre, setSelectedGenre] = useState("");
+  const [filteredMovies, setFilteredMovies] = useState(movies);
+
+  //filtro i generi
+  const genres = movies.map((movie) => movie.genre);
+  //filtro i generi unici
+  const uniqueGenres = genres.filter((genre, index) => {
+    return genres.indexOf(genre) === index;
+  });
+
+  //filtrare i film
+  // const filteredMovie = selectedGenre
+  //   ? movies.filter((movie) => movie.genre === selectedGenre)
+  //   : movies;
+
+  return (
+    <div className="container">
+      <h1>Film disponibili</h1>
+      <div className="input-group mb-3">
+        <button className="input-group-text btn btn-secondary">Cerca</button>
+        <select className="form-select">
+          <option>Seleziona un genere</option>
+          {uniqueGenres.map((genre) => (
+            <option value={genre} key={genre}>
+              {genre}
+            </option>
+          ))}
+        </select>
+      </div>
+      {/* qui abbiamo le card con i film */}
+      <hr />
+    </div>
+  );
 }
