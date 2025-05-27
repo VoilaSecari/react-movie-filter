e88ebba460b2956fd3e4627a27dae2487be699de
VoilaSecari <lifeaslaurafilip@gmail.com>
2025-05-28 00:52:55 +0200

    conclusione

--- src/App.jsx
+++ src/App.jsx
@@ -17,16 +17,32 @@ export default function App() {
 
   //filtrare i film
   // const filteredMovie = selectedGenre
-  //   ? movies.filter((movie) => movie.genre === selectedGenre)
-  //   : movies;
+  // ? movies.filter((movie) => movie.genre === selectedGenre)
+  //   : movies; movie
+
+  useEffect(() => {
+    if (selectedGenre) {
+      setFilteredMovies(
+        movies.filter((movie) => movie.genre === selectedGenre)
+      );
+    } else {
+      setFilteredMovies(movies);
+    }
+  }, [selectedGenre]);
 
   return (
     <div className="container">
       <h1>Film disponibili</h1>
       <div className="input-group mb-3">
-        <button className="input-group-text btn btn-secondary">Cerca</button>
-        <select className="form-select">
-          <option>Seleziona un genere</option>
+        <span className="input-group-text">Cerca</span>
+        <select
+          className="form-select"
+          id="genreSelect"
+          value={selectedGenre}
+          onChange={(e) => setSelectedGenre(e.target.value)}
+        >
+          <option value="">Seleziona un genere...</option>
+          {/* genera i valori del filtro */}
           {uniqueGenres.map((genre) => (
             <option value={genre} key={genre}>
               {genre}
@@ -36,6 +52,22 @@ export default function App() {
       </div>
       {/* qui abbiamo le card con i film */}
       <hr />
+      <div className="container">
+        <div className="row g-3">
+          {filteredMovies.map((movie, index) => (
+            <div className="col-4" key={index}>
+              <div className="card">
+                <div className="card-header">
+                  <h4 className="mb-0 card-title">{movie.title}</h4>
+                </div>
+                <div className="card-body">
+                  <p className="mb-0 card-text">{movie.genre}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
