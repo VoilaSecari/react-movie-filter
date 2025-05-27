e62938eb1d4983bf4a385391f2a041ed1aba4f9c
VoilaSecari <lifeaslaurafilip@gmail.com>
2025-05-27 21:52:11 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/css/index.css";
+
+export default function App() {
+  return <h1>Bella lì</h1>;
+}
